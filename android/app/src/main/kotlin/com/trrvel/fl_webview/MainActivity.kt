35d555d28ee203453bdccd9f6b582edd18a1508b
Daryl <darylwong@primustech.com.sg>
2020-12-21 19:06:40 +0800

    m

---
+++ android/app/src/main/kotlin/com/trrvel/fl_webview/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.trrvel.fl_webview
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+
+Future main() async {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => new _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: InAppWebViewPage());
+  }
+}
+
+class InAppWebViewPage extends StatefulWidget {
+  @override
+  _InAppWebViewPageState createState() => new _InAppWebViewPageState();
+}
+
+class _InAppWebViewPageState extends State<InAppWebViewPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("InAppWebView")),
+      body: Container(
+        child: Column(children: <Widget>[
+          Expanded(
+            child: Container(
+              child: InAppWebView(
+                initialUrl: "https://musing-borg-8fb7f1.netlify.app/",
+              ),
+            ),
+          ),
+        ]),
+      ),
+    );
+  }
+}
